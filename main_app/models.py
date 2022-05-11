dda6850aaf98a412f5a282516298e8cad6da3a0c
tom-gordon13 <tombcgordon@gmail.com>
2022-05-11 09:54:15 -0600

    Add Servicing model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
+
+
+# Types of servicing appointments
+TYPES = (
+    ('G', 'General Maintenance'),
+    ('R', 'Mechanical Repairs'),
+    ('S', 'Strap Replacement')
+)
 
 class Watch(models.Model):
     make = models.CharField(max_length=100)
@@ -12,6 +19,19 @@ class Watch(models.Model):
         return f'{self.make} - {self.model} ({self.id})'
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs= {
-            'watch_id': self.id
-        })
+        return reverse('detail', kwargs= {'watch_id': self.id})
+
+
+class Servicing(models.Model):
+    watch = models.ForeignKey(Watch, on_delete=models.CASCADE)
+    date = models.DateField()
+    cost = models.PositiveIntegerField()
+    type = models.CharField(
+        max_length=1,
+        choices=TYPES,
+        default=TYPES[0][0]
+    )
+
+
+    def __str__(self):
+        return f'{self.type} - ${self.cost}'
