e7a59db6b1171756b5b440e739d2533450e4bac2
tom-gordon13 <tombcgordon@gmail.com>
2022-05-11 21:12:20 -0600

    Add M:M display functionality with components

--- main_app/models.py
+++ main_app/models.py
@@ -6,14 +6,49 @@ from django.urls import reverse
 TYPES = (
     ('G', 'General Maintenance'),
     ('R', 'Mechanical Repairs'),
-    ('S', 'Strap Replacement')
+    ('S', 'Strap Replacement'),
 )
+
+TYPES_COMP = (
+    ('M', 'Movement'),
+    ('S', 'Strap'),
+    ('C', 'Crystal'),
+)
+
+SUBTYPES_COMP = (
+        ('MQ', 'Quartz'),
+        ('MA', 'Automatic'),
+        ('MM', 'Manual'),
+        ('SL', 'Leather'),
+        ('SM', 'Metal'),
+        ('CA', 'Acrylic'),
+        ('CM', 'Mineral'),
+        ('CS', 'Sapphire'),
+)
+
+class Component(models.Model):
+    type_comp = models.CharField(
+        max_length=1,
+        choices=TYPES_COMP,
+        default=TYPES[0][0]
+    )
+    subtype_comp = models.CharField(
+      max_length=2,
+      choices=SUBTYPES_COMP,
+    )
+
+    def __str__(self):
+        return f'{self.get_type_comp_display()}'
+
+ 
+
 
 class Watch(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
     price = models.PositiveIntegerField()
+    components = models.ManyToManyField(Component)
 
     def __str__(self):
         return f'{self.make} - {self.model} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 
 from main_app.forms import ServicingForm
 
-from .models import Watch
+from .models import Watch, Component
 
 def home(request):
   return render(request, 'home.html')
@@ -19,10 +19,13 @@ def watch_index(request):
 
 def watch_detail(request, watch_id):
   watch = Watch.objects.get(pk=watch_id)
+  id_list = watch.components.all().values_list('id')
+  missing_comp = Component.objects.exclude(id__in=id_list)
   servicing_form = ServicingForm()
   return render(request, 'watches/detail.html', {
     'watch': watch,
-    'servicing_form': servicing_form
+    'servicing_form': servicing_form, 
+    'components': missing_comp,
   })
 
 def add_servicing(request, watch_id):
@@ -33,11 +36,14 @@ def add_servicing(request, watch_id):
     new_servicing.save()
   return redirect('detail', watch_id=watch_id)
 
+def assoc_toy(request, watch_id, component_id):
+  Watch.objects.get(id=watch_id).components.add(component_id)
+  return redirect('detail', watch_id=watch_id)
 
 #Class-based View
 class WatchCreate(CreateView):
   model = Watch
-  fields = '__all__'
+  fields = ['make', 'model', 'year', 'price']
 
 class WatchUpdate(UpdateView):
   model = Watch
